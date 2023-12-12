e7439278d768009b81c719fece4dc636617bb0cf
Karl Johan Ansari-Önnestam <karl.johan.ansari-onnestam@einride.tech>
2023-12-12 08:45:01 +0100

    chore: format using sgmdformat instead of markdownfmt

--- .sage/main.go
+++ .sage/main.go
@@ -12,7 +12,7 @@ import (
 	"go.einride.tech/sage/tools/sggoreview"
 	"go.einride.tech/sage/tools/sggosemanticrelease"
 	"go.einride.tech/sage/tools/sggovulncheck"
-	"go.einride.tech/sage/tools/sgmarkdownfmt"
+	"go.einride.tech/sage/tools/sgmdformat"
 	"go.einride.tech/sage/tools/sgyamlfmt"
 )
 
@@ -65,7 +65,7 @@ func GoVulnCheck(ctx context.Context) error {
 
 func FormatMarkdown(ctx context.Context) error {
 	sg.Logger(ctx).Println("formatting Markdown files...")
-	return sgmarkdownfmt.Command(ctx, "-w", ".").Run()
+	return sgmdformat.Command(ctx).Run()
 }
 
 func ConvcoCheck(ctx context.Context) error {
